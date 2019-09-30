30c887fcf454d6465361b21689ceff5811c32f54
B533P-14\533 <Olivier.allaire00@outlook.fr>
2019-09-30 15:35:03 -0400

    fini

---
+++ Cours12/Assets/Script/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    public void Start()
+    {
+        int numOfMusicPlayer = FindObjectsOfType<MusicPlayer>().Length;
+        if (numOfMusicPlayer > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Cours12/Assets/laugh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laugh : MonoBehaviour {
+    AudioSource Audio;
+
+	// Use this for initialization
+	void Start () {
+        Audio = FindObjectOfType<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Joueur")
+        {
+            Audio.Play();
+        }
+    }
+}
